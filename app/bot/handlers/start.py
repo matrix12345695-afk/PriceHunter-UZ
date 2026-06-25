a69ce2f9f58576289dba34ff10d9b9b3d865804c
matrix12345695-afk <matrix12345695@gmail.com>
2026-06-25 14:38:40 +0500

    Update start.py

--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -2,13 +2,22 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from app.bot.keyboards.main_menu import main_menu
+
 router = Router()
 
 
 @router.message(CommandStart())
 async def start(message: Message):
     await message.answer(
-        "👋 <b>Добро пожаловать в PriceHunter UZ</b>\n\n"
-        "🚀 Самый умный поиск цен в Узбекистане.\n\n"
-        "Пока проект находится в разработке."
+        f"""
+👋 <b>Добро пожаловать в PriceHunter UZ</b>
+
+Привет, <b>{message.from_user.full_name}</b>!
+
+🚀 Мы поможем найти лучшие цены в Узбекистане.
+
+Выберите действие ниже.
+""",
+        reply_markup=main_menu,
     )
