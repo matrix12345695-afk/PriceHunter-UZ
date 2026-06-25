66a15550430f5d85eb2418355cf2ca0f156a4402
matrix12345695-afk <matrix12345695@gmail.com>
2026-06-25 15:45:20 +0500

    Update olcha_http.py

--- app/parsers/providers/olcha_http.py
+++ app/parsers/providers/olcha_http.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+
+from app.extractors.olcha import OlchaExtractor
 from app.parsers.base_http import BaseHttpParser
 
 
@@ -5,8 +8,21 @@ class OlchaHttpParser(BaseHttpParser):
 
     BASE_URL = "https://olcha.uz"
 
+    def __init__(self):
+        super().__init__()
+        self.extractor = OlchaExtractor()
+
     async def search(self, query: str):
 
-        raise NotImplementedError(
-            "Research in progress."
+        url = f"{self.BASE_URL}/ru/search?search={query}"
+
+        response = await self.client.get(url)
+
+        response.raise_for_status()
+
+        soup = BeautifulSoup(
+            response.text,
+            "lxml",
         )
+
+        return self.extractor.extract_search(soup)
