e1776d871a2bce52b97c135477b362c34ae4f14f
matrix12345695-afk <matrix12345695@gmail.com>
2026-06-25 14:41:54 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,12 +3,17 @@ import asyncio
 from app.bot.bot import bot
 from app.bot.dispatcher import dp
 from app.core.logger import get_logger
+from app.database.init_db import init_database
 
 logger = get_logger()
 
 
 async def main():
     logger.info("Starting PriceHunter UZ...")
+
+    await init_database()
+
+    logger.success("Bot started.")
 
     await dp.start_polling(bot)
 
