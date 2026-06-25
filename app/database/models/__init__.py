54a628ab77b3ae0659e2669a208031596b1d7251
matrix12345695-afk <matrix12345695@gmail.com>
2026-06-25 14:48:07 +0500

    Update __init__.py

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -1,5 +1,11 @@
 from app.database.models.user import User
+from app.database.models.shop import Shop
+from app.database.models.category import Category
+from app.database.models.product import Product
 
 __all__ = [
     "User",
+    "Shop",
+    "Category",
+    "Product",
 ]
