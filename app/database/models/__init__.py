d0f14e43aed97f31dc31a275253e2b2299059dcd
matrix12345695-afk <matrix12345695@gmail.com>
2026-06-25 14:50:13 +0500

    Update __init__.py

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -2,10 +2,16 @@ from app.database.models.user import User
 from app.database.models.shop import Shop
 from app.database.models.category import Category
 from app.database.models.product import Product
+from app.database.models.product_alias import ProductAlias
+from app.database.models.price import Price
+from app.database.models.price_history import PriceHistory
 
 __all__ = [
     "User",
     "Shop",
     "Category",
     "Product",
+    "ProductAlias",
+    "Price",
+    "PriceHistory",
 ]
