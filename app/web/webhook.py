2840753119683867face093cc9b7a090e8c79c96
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-17 17:44:44 +0500

    Update webhook.py

--- app/web/webhook.py
+++ app/web/webhook.py
@@ -5,6 +5,7 @@ from aiogram.types import Update
 
 from app.bot.bot import bot
 from app.bot.dispatcher import dp
+from app.core.logger import logger
 from app.core.settings import settings
 
 router = APIRouter()
@@ -12,21 +13,22 @@ router = APIRouter()
 
 @router.post("/webhook")
 async def telegram_webhook(request: Request):
-    """
-    Endpoint, на который Telegram будет отправлять обновления.
-    """
 
-    # Если используем секретный токен Telegram
     if settings.WEBHOOK_SECRET:
         secret = request.headers.get("X-Telegram-Bot-Api-Secret-Token")
 
         if secret != settings.WEBHOOK_SECRET:
-            raise HTTPException(status_code=403, detail="Invalid secret token")
+            logger.warning("Invalid webhook secret")
+            raise HTTPException(status_code=403)
 
     data = await request.json()
+
+    logger.info(f"Webhook update received: {data.get('update_id')}")
 
     update = Update.model_validate(data)
 
     await dp.feed_update(bot, update)
+
+    logger.success("Update processed")
 
     return {"ok": True}
