77909b51bba368305d364b33ae1bd75992ac3b8f
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-20 14:30:38 +0500

    Update uzum.py

--- app/providers/uzum.py
+++ app/providers/uzum.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from app.domain.product_dto import ProductDTO
+from typing import Any
+
+from app.core.http import http
 from app.providers.base import BaseProvider
 
 
@@ -8,35 +10,78 @@ class UzumProvider(BaseProvider):
 
     name = "Uzum"
 
+    GRAPHQL_URL = "https://graphql.uzum.uz/"
+
+    async def search_raw(
+        self,
+        query: str,
+    ) -> dict[str, Any]:
+
+        payload = {
+            "operationName": "MakeSearch_ItemsAndFilters",
+            "variables": {
+                "queryInput": {
+                    "text": query,
+                    "showAdultContent": "NONE",
+                    "filters": [],
+                    "sort": "BY_RELEVANCE_DESC",
+                    "pagination": {
+                        "offset": 0,
+                        "limit": 48,
+                    },
+                    "correctQuery": True,
+                    "getFastCategories": True,
+                    "fastCategoriesLimit": 11,
+                    "fastCategoriesLevelOffset": 2,
+                    "getPromotionItems": True,
+                    "getFastFacets": False,
+                    "fastFacetsLimit": 0,
+                }
+            },
+            "query": """
+query MakeSearch_ItemsAndFilters(
+  $queryInput: MakeSearchQueryInput!
+) {
+  makeSearch(query: $queryInput) {
+    queryText
+    total
+    catalogCards {
+      id
+      title
+      product {
+        id
+      }
+    }
+  }
+}
+""",
+        }
+
+        response = await http.post(
+            self.GRAPHQL_URL,
+            json=payload,
+        )
+
+        response.raise_for_status()
+
+        return response.json()
+
     async def search(
         self,
         query: str,
         page: int = 1,
-    ) -> list[ProductDTO]:
-
-        #
-        # Реализуем позже
-        #
-
+    ):
         return []
 
     async def get_product(
         self,
         external_id: str,
-    ) -> ProductDTO | None:
-
+    ):
         return None
 
-    async def healthcheck(
-        self,
-    ) -> bool:
-
+    async def healthcheck(self) -> bool:
         try:
-
-            result = await self.search("iphone")
-
-            return len(result) > 0
-
+            await self.search_raw("iphone")
+            return True
         except Exception:
-
             return False
