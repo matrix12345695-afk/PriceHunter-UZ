48e03e929e1a35a1cf74a867bc9d76a1e8bef153
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-20 10:28:18 +0500

    Update store.py

--- app/models/store.py
+++ app/models/store.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -17,7 +19,7 @@ class Store(Base):
         unique=True,
     )
 
-    products = relationship(
-        "Product",
-        backref="store",
+    products: Mapped[list["Product"]] = relationship(
+        back_populates="store",
+        cascade="all, delete-orphan",
     )
